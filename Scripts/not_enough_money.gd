8435f05dda4c3a29530094f2037ef6bae9098952
Kojithecat <rulokoji@gmail.com>
2025-05-31 11:00:49 +0200

    Label not enough money

---
+++ Scripts/not_enough_money.gd
@@ -0,0 +1,11 @@
+extends Label
+
+func _ready():
+	add_theme_font_size_override("font_size", 32)  # Bigger text
+	add_theme_color_override("font_color", Color.RED)
+	add_theme_color_override("font_outline_color", Color.DARK_RED)
+	add_theme_constant_override("outline_size", 4)
+	update_display()
+	
+func update_display():
+	text = "Not enough money to buy that place!"
